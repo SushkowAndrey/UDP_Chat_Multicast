4ae21265267036c2a317441bbba797f73989597f
Starinin Andrey <starininandrey@gmail.com>
2021-12-12 12:50:40 +0300

    Add SendAsync, ReceiveAsync, ReceiveMulticastAsync

--- UDP_Chat_Multicast_Lib/Udp.cs
+++ UDP_Chat_Multicast_Lib/Udp.cs
@@ -1,8 +1,28 @@
-﻿using System;
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
 
 namespace UDP_Chat_Multicast_Lib
 {
-    public class Udp
+    public static class Udp
     {
+        public static async Task SendAsync(byte[] data, string ip, int port)
+        {
+            using var sender = new UdpClient();
+            await sender.SendAsync(data, data.Length, ip, port);
+        }
+
+        public static async Task<UdpReceiveResult> ReceiveAsync(int port)
+        {
+            using var receiver = new UdpClient(port);
+            return await receiver.ReceiveAsync();
+        }
+
+        public static async Task<UdpReceiveResult> ReceiveMulticastAsync(int port, string multicast_ip)
+        {
+            using var receiver = new UdpClient(port);
+            receiver.JoinMulticastGroup(IPAddress.Parse(multicast_ip));
+            return await receiver.ReceiveAsync();
+        }
     }
 }
