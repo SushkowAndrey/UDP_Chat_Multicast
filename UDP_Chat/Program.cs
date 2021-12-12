2cc1c490f110bc60a5368450988e32fd6a0fe96a
Starinin Andrey <starininandrey@gmail.com>
2021-12-12 13:17:44 +0300

    Update

--- UDP_Chat/Program.cs
+++ UDP_Chat/Program.cs
@@ -1,12 +1,59 @@
 ﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using UDP_Chat_Multicast_Lib;
 
 namespace UDP_Chat
 {
     class Program
     {
-        static void Main(string[] args)
+        private static string multicast_ip;
+        private static int port;
+        private static string user_name;
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Введите ip-адрес рассылки: ");
+            multicast_ip = Console.ReadLine();
+            Console.Write("Введите порт: ");
+            port = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите имя: ");
+            user_name = Console.ReadLine();
+
+            Task.Run(Receive);
+
+            Send();
+        }
+
+        static async Task Send()
+        {
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("Введите сообщение: ");
+                var message = Console.ReadLine();
+                Console.ResetColor();
+
+                var data = Encoding.Unicode.GetBytes($"{user_name}|{message}");
+                await Udp.SendAsync(data, multicast_ip, port);
+            }
+        }
+
+        static async Task Receive()
+        {
+            while (true)
+            {
+                var result = await Udp.ReceiveMulticastAsync(port, multicast_ip);
+                var data = result.Buffer;
+                var temp = Encoding.Unicode.GetString(data);
+                var t = temp.Split('|');
+                var user = t[0];
+                var message = t[1];
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.Write($"{user} -> ");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(message);
+                Console.ResetColor();
+            }
         }
     }
 }
